1ed622f4223da9e77087fa86eb65cbdc25167c0c
Anderson Pimentel <apds.anderson@gmail.com>
2021-03-14 23:47:52 -0400

    feat: Add ec2 resource

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
-  version = "~> 2.0"
-}
 
 terraform {
+  required_providers {
+    aws = {
+      version = "~> 2.0"
+    }
+  }
   backend "s3" {
     bucket = "terraform-ltips"
     key = "terraform-test.tfstate"
     region = "us-east-1"
   }
+}
+provider "aws" {
+  region = "us-east-1"
+  # version = "~> 2.0"
 }
