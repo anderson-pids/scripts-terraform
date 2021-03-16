4447564ede591077f0061368cf77b5cccfc31d9c
Anderson Pimentel <apds.anderson@gmail.com>
2021-03-16 08:24:50 -0400

    feat: Add data block to ec2

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,14 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu - canonical
+}
+
 resource "aws_instance" "web" {
   ami = "ami-0885b1f6bd170450c"
   instance_type = "t2.micro"
