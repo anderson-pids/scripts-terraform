c48b7e23ab5b63ed75a253cc7812bf773e23cc51
Anderson Pimentel <apds.anderson@gmail.com>
2021-07-12 20:18:50 -0400

    feat(ltips): ignore output of plan and filter data for ami of ec2

--- src/ltips/aula1/ec2.tf
+++ src/ltips/aula1/ec2.tf
@@ -1,5 +1,18 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # ubuntu
+}
+
+
 resource "aws_instance" "web" {
-  ami = "ami-0885b1f6bd170450c"
+  # ami = "ami-0885b1f6bd170450c"
+  ami = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
